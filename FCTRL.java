fe8285922751d6264552ce04217f6e9c0efb2f8c
Kshitij jain <mail.kshitij.jain@gmail.com>
2013-02-13 14:46:21 +0530

    Spoj problem FCTRL

---
+++ FCTRL.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+class Test {
+
+    public static void main(String[] args) {
+
+        Scanner in = new Scanner(System.in);
+        int cases = in.nextInt();
+
+        while (cases != 0) {
+
+            System.out.println("" + getZeros(in.nextInt()));
+
+            cases--;
+        }
+
+    }// main ends here
+
+    private static int getZeros(int nextInt) {
+
+        int trail = 0;
+
+        for (int i = 5; i <= nextInt; i *= 5) {
+
+            trail += nextInt / i;
+
+        }
+        return trail;
+    }
+}// class ends here
+
